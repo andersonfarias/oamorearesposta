d3ffdeac1659fe231c7c5f771107bdbb32b68d8d
Olivan Aires <olivanaires@gmail.com>
2016-06-19 20:23:50 -0300

    added attribut name to date field

--- app/inputs/datepicker_input.rb
+++ app/inputs/datepicker_input.rb
@@ -1,13 +1,14 @@
 class DatepickerInput < SimpleForm::Inputs::Base
   def input
-   template.content_tag(:div, class: 'input-group date form_datetime') do
-     template.concat @builder.text_field(attribute_name, input_html_options)
-     template.concat span_table
+    @value_input = "#{input_options[:class]}[#{attribute_name}]"
+    template.content_tag(:div, class: 'input-group date form_datetime') do
+      template.concat @builder.text_field(attribute_name, input_html_options)
+      template.concat span_table
    end
  end
 
  def input_html_options
-   super.merge({class: 'form-control', value: ""})
+   super.merge({class: 'form-control', name: "#{@value_input}"})
  end
 
   def span_table
