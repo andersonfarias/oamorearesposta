ec4c78b1559a60b1c6edf78e1be9b95b40e1543b
Olivan Aires <olivanaires@gmail.com>
2016-06-19 15:24:08 -0300

    created w_radiobox and changed w_checkbox to add flat class iputs refactored first_contact_file view to use the wrappers changed default item wrapper tag to wrap each item in a collection of radio/check boxes with a div

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -86,17 +86,38 @@ SimpleForm.setup do |config|
 		end
 	end
 
-	config.wrappers :w_checkboxes, tag: 'div', html: { style: 'display: flex; flex-flow: column nowrap;' }, error_class: 'has-error' do |b|
-    b.use :html5
-    b.optional :readonly
+	config.wrappers :w_radioboxes, tag: 'div', error_class: 'has-error' do |b|
+		b.use :html5
+		b.use :placeholder
+		b.optional :maxlength
+		b.optional :pattern
+		b.optional :min_max
+		b.optional :readonly
 
-    b.use :label
+		b.wrapper :tag => 'label', :class => 'control-label col-md-12 col-sm-12 col-xs-12' do |bb|
+			bb.use :label_text
+		end
 
-    b.wrapper tag: 'div' do |ba|
-      ba.use :input
-      ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
-    end
+		b.wrapper :tag => 'div' do | ba |
+			ba.use :input, class: 'flat'
+		end
+  end
+
+	config.wrappers :w_checkboxes, tag: 'div', error_class: 'has-error' do |b|
+		b.use :html5
+		b.use :placeholder
+		b.optional :maxlength
+		b.optional :pattern
+		b.optional :min_max
+		b.optional :readonly
+
+		b.wrapper :tag => 'label', :class => 'control-label col-md-12 col-sm-12 col-xs-12' do |bb|
+			bb.use :label_text
+		end
+
+		b.wrapper :tag => 'div' do | ba |
+			ba.use :input, class: 'flat'
+		end
   end
 
 	# The default wrapper to be used by the FormBuilder.
@@ -106,7 +127,7 @@ SimpleForm.setup do |config|
 	# Defaults to :nested for bootstrap config.
 	#   inline: input + label
 	#   nested: label > input
-	config.boolean_style = :nested
+	config.boolean_style = :inline
 
 	# Default class for buttons
 	config.button_class = 'btn'
@@ -139,7 +160,7 @@ SimpleForm.setup do |config|
 
 	# You can wrap each item in a collection of radio/check boxes with a tag,
 	# defaulting to :span.
-	# config.item_wrapper_tag = :span
+	config.item_wrapper_tag = :div
 
 	# You can define a class to use in all item wrappers. Defaulting to none.
 	# config.item_wrapper_class = nil
