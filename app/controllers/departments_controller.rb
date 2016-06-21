9701ea3033eb1567328a3dbfc7d463b51d5654c5
Olivan Aires <olivanaires@gmail.com>
2016-06-21 13:53:07 -0300

    created default action success message internationalization

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -18,7 +18,8 @@ class DepartmentsController < ApplicationController
     authorize current_user
     @department = Department.new(department_params)
     if @department.save
-      redirect_to @department, notice: 'Criado com sucesso.'
+      redirect_to @department,
+        notice: t('controllers.actions.create.success', model: Department.model_name.human(count: 1))
     end
   end
 
@@ -28,13 +29,15 @@ class DepartmentsController < ApplicationController
   def update
     authorize current_user
     if @department.update(department_params)
-      redirect_to @department, notice: 'Atualizado com sucesso.'
+      redirect_to @department,
+        notice: t('controllers.actions.update.success', model: Department.model_name.human(count: 1))
     end
   end
 
   def destroy
     if @department.destroy
-      redirect_to departments_path, notice: 'Deletado com sucesso.'
+      redirect_to departments_path,
+        notice: t('controllers.actions.destroy.success', model: Department.model_name.human(count: 1))
     end
   end
 
