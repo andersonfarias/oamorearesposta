f09a839e8f14302f8374fc7e4e8bb041bf9aa367
Anderson Farias <anderson.faryas@gmail.com>
2016-06-22 21:54:21 -0300

    Fixing migration for pgadmin database when changing from varchar to integer

--- db/migrate/20160615153715_change_people_age_type.rb
+++ db/migrate/20160615153715_change_people_age_type.rb
@@ -1,5 +1,5 @@
 class ChangePeopleAgeType < ActiveRecord::Migration
-  def change
-    change_column :people, :age, :integer
-  end
+	def change
+		change_column :people, :age, 'integer USING CAST(column_name AS integer)'
+	end
 end
