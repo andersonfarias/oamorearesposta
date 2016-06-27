b5c1fe15c405d892c037ccfbeb9b9b91c55614be
Olivan Aires <olivanaires@gmail.com>
2016-06-27 13:28:48 -0300

    set person email in create admin user service

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -2,7 +2,7 @@ class CreateAdminService
 	def call
 		user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
 			puts 'CREATING ADMIN USER'
-			user.person = Person.create( first_name: Rails.application.secrets.admin_name )
+			user.person = Person.create( first_name: Rails.application.secrets.admin_name, email: Rails.application.secrets.admin_email )
 			user.password = Rails.application.secrets.admin_password
 			user.password_confirmation = Rails.application.secrets.admin_password
 			if user.confirm! && user.admin!
