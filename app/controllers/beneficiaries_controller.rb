f7f9cebd3a6f2c9f7c6ba01212d8ad54e817f8b3
Olivan Aires <olivanaires@gmail.com>
2016-07-03 11:36:38 -0300

    added search filters to beneficiary index view

--- app/controllers/beneficiaries_controller.rb
+++ app/controllers/beneficiaries_controller.rb
@@ -9,11 +9,18 @@ class BeneficiariesController < ApplicationController
 	end
 
 	def index
-		@beneficiaries = Beneficiary.all.paginate( :page => params[ :page ] )
+		department_name = params[:department_name].to_s
+		beneficiary_first_name = params[:beneficiary_first_name].to_s
+		beneficiary_last_name = params[:beneficiary_last_name].to_s
+		@beneficiaries = Beneficiary.joins(:department, :person)
+			.where("people.first_name LIKE '%#{beneficiary_first_name}%'
+				AND people.last_name LIKE '%#{beneficiary_last_name}%' 
+				AND departments.name LIKE '%#{department_name}%'" )
+			.all.paginate( :page => params[ :page ] )
 	end
 
 	private
-	
+
 	def set_beneficiary
 		@beneficiary = Beneficiary.find(params[:id])
 	end
