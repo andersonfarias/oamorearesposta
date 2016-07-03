4e7b7e81b7447151e84ea6f27c7ce2b8724591c0
Olivan Aires <olivanaires@gmail.com>
2016-07-03 12:51:51 -0300

    moved query from beneficiary controller to model

--- app/controllers/beneficiaries_controller.rb
+++ app/controllers/beneficiaries_controller.rb
@@ -9,14 +9,7 @@ class BeneficiariesController < ApplicationController
 	end
 
 	def index
-		department_name = params[:department_name].to_s
-		beneficiary_first_name = params[:beneficiary_first_name].to_s
-		beneficiary_last_name = params[:beneficiary_last_name].to_s
-		@beneficiaries = Beneficiary.joins(:department, :person)
-			.where("people.first_name LIKE '%#{beneficiary_first_name}%'
-				AND people.last_name LIKE '%#{beneficiary_last_name}%' 
-				AND departments.name LIKE '%#{department_name}%'" )
-			.all.paginate( :page => params[ :page ] )
+		@beneficiaries = Beneficiary.by_name_and_department(params).paginate( :page => params[ :page ] )
 	end
 
 	private
--- app/models/beneficiary.rb
+++ app/models/beneficiary.rb
@@ -7,4 +7,11 @@ class Beneficiary < ActiveRecord::Base
 
   validates_associated :person, :department, :first_contact_file
   validates_presence_of :person, :department
+
+  def self.by_name_and_department params 
+    Beneficiary.joins(:department, :person)
+			.where("people.first_name LIKE '%#{params[:beneficiary_first_name].to_s}%'
+				AND people.last_name LIKE '%#{params[:beneficiary_last_name].to_s}%'
+				AND departments.name LIKE '%#{params[:department_name].to_s}%'" )
+  end
 end
