5d5f44adfecbd1236a59ed7f9da62290308bf8d4
Anderson Farias <anderson.faryas@gmail.com>
2016-07-05 11:56:51 -0300

    Continuee fixing the show view of first contact file

--- app/controllers/beneficiaries_controller.rb
+++ app/controllers/beneficiaries_controller.rb
@@ -9,12 +9,7 @@ class BeneficiariesController < ApplicationController
 	end
 
 	def index
-		root = Beneficiary.joins( :person, :department )
-		root = root.where( "LOWER( people.first_name ) LIKE LOWER('%#{params[ :beneficiary_first_name ].to_s}%')" ) unless params[ :beneficiary_first_name ].blank?
-		root = root.where( "LOWER( people.last_name ) LIKE LOWER('%#{params[ :beneficiary_last_name ].to_s}%')" ) unless params[ :beneficiary_last_name ].blank?
-		root = root.where( "LOWER( departments.name ) LIKE LOWER('%#{params[ :department_name ].to_s}%')" ) unless params[ :department_name ].blank?
-
-		@beneficiaries = root.paginate( :page => params[ :page ] )
+		@beneficiaries = Beneficiary.by_name_and_department( params ).paginate( :page => params[ :page ] )
 	end
 
 	private
