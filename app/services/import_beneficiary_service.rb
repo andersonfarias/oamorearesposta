27d996bbf2a36953ec4521dd2744d4177dd77384
Olivan Aires <olivanaires@gmail.com>
2016-07-09 13:37:07 -0300

    beneficiary migration database

--- app/services/import_beneficiary_service.rb
+++ app/services/import_beneficiary_service.rb
@@ -3,25 +3,52 @@ require 'csv'
 class ImportBeneficiaryService
 	def call
 		FirstContactFile.destroy_all
-		list = []
+
+		aux = 0
 		CSV.foreach("#{Rails.root}/app/services/migration_files/beneficiaries.csv") do |r|
 		 	row = r.first
-			beneficiary = row.split(/\|/)
+			beneficiary = row.split("|")
+			aux += 1
 			
 			family_structure = if beneficiary[21] == "Outros" then 5 else beneficiary[21] end
+			
 			answer = []
-			beneficiary[61].split(";").each {|v| answer << FirstContactFile.answer.values.at(v.to_i-1)}
+			if beneficiary[61] && !beneficiary[61].empty?
+				beneficiary[61].split(";").each {|v| answer << FirstContactFile.answer.values.at(v.to_i-1)}
+			else
+				answer << FirstContactFile.answer.values.at(7)
+			end
+			
 			petitions = []
-			beneficiary[63].split(";").each {|v| petitions << FirstContactFile.petitions.values.at(v.to_i-1)}
+			if beneficiary[63] && !beneficiary[63].empty?
+				beneficiary[63].split(";").each {|v| petitions << FirstContactFile.petitions.values.at(v.to_i-1)} 
+			else
+				petitions << FirstContactFile.petitions.values.at(24)
+			end
+
 			results = []
-			beneficiary[62].split(";").each {|v| results << FirstContactFile.results.values.at(v.to_i-1)}
+			if beneficiary[62] && !beneficiary[62].empty?
+				beneficiary[62].split(";").each {|v| results << FirstContactFile.results.values.at(v.to_i-1)} 
+			else
+				results << FirstContactFile.results.values.at(4)
+			end
+
 			first_contact_conditions = []
-			beneficiary[87].split(";").each {|v| first_contact_conditions << FirstContactFile.first_contact_conditions.values.at(v.to_i-1)}
+			if beneficiary[87] && !beneficiary[87].empty?
+				beneficiary[87].split(";").each {|v| first_contact_conditions << FirstContactFile.first_contact_conditions.values.at(v.to_i-1)}
+			else
+				first_contact_conditions << FirstContactFile.first_contact_conditions.values.at(30)
+			end
+
 			education_levels = []
 			education_levels += FirstContactFile.education_levels.values[0..beneficiary[50].to_i] if beneficiary[50].to_i < 8
 			education_levels << FirstContactFile.education_levels.values.at(9) if beneficiary[82] == "'SIM'"
 			education_levels << FirstContactFile.education_levels.values.at(9) if beneficiary[83] == "'SIM'"
-			beneficiary[85].split(";").each {|m| education_levels << FirstContactFile.education_levels.values.at(m.delete("'").to_i)}
+			if beneficiary[85] && !beneficiary[85].empty? && education_levels.empty?
+				beneficiary[85].split(";").each {|m| education_levels << FirstContactFile.education_levels.values.at(m.delete("'").to_i)}
+			else
+				education_levels << FirstContactFile.education_levels.values.at(0)
+			end
 			
 			f = FirstContactFile.create(
 				id: beneficiary[0].to_i,
@@ -52,42 +79,44 @@ class ImportBeneficiaryService
 				first_contact_conditions: first_contact_conditions,
 				beneficiary: Beneficiary.new(
 					id: beneficiary[0].to_i,
-					department_id: beneficiary[2].to_i,
+					department_id: beneficiary[1].to_i,
 					person: Person.new(
-						id: beneficiary[0].to_i,
-						first_name: beneficiary[7].split(" ", 2)[1],
-						last_name: beneficiary[7].split(" ", 2)[1],
+						first_name: beneficiary[7].split(" ", 2)[0].delete("'"),
+						last_name: if beneficiary[7].split(" ", 2)[1] then beneficiary[7].split(" ", 2)[1].delete("'") else "" end,
 						birthdate: beneficiary[10],
 						age: beneficiary[11],
 						gender: beneficiary[14].to_i-1,
-						email: if beneficiary[81].empty? then nil else beneficiary[81] end,
+						email: if beneficiary[81] == "''" then "nao_informado@ong.com.br" else beneficiary[81] end,
 						address: Address.new(
-							id: beneficiary[0].to_i,
 							street: beneficiary[76],
 							neighborhood: beneficiary[78],
 							number: beneficiary[77],
 							cep: beneficiary[75],
 							complement: beneficiary[79],
-							city_id: beneficiary[100].to_i
-						)
+							city_id: if beneficiary[99] == "''" || beneficiary[99].to_i == 0 then 3582 else beneficiary[99].to_i end
+						),
+						phones: [
+							Phone.new()
+						]
 					)
 				), 
 				contact_source: Person.new(
-					id: beneficiary[0].to_i,
 					first_name: "Não Informado",
 					age: 0,
-					gender: 0
+					gender: 0,
+					phones: [
+						Phone.new()
+					]
 				),
 				support: Person.new(
-					id: beneficiary[0].to_i,
-					first_name: "Não Informado"
+					first_name: "Não Informado",
+					phones: [
+						Phone.new()
+					]
 				)
 			)
-			list << f
-			# p f.errors.messages
-			p beneficiary[2].to_i
+			p "Linha: #{aux} - Errro: #{f.errors.messages}" if !f.errors.empty?
+			p "===========================================" if !f.errors.empty?
 		end
-		
-		p list.first.errors.messages
 	end
 end
