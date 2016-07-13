689754559b27c3f52e66cd17a658ae5c80df45a3
Olivan Aires <olivanaires@gmail.com>
2016-07-12 22:36:53 -0300

    added second order by id desc in clinic treatment. for events on the same day.

--- app/models/clinic_treatment.rb
+++ app/models/clinic_treatment.rb
@@ -2,5 +2,5 @@ class ClinicTreatment < Diary
   validates_presence_of :date, :description
   validates_associated :beneficiary, :user
 
-  scope :by_beneficiary, ->(id) { where(beneficiary_id: id).order(date: :desc)  }
+  scope :by_beneficiary, ->(id) { where(beneficiary_id: id).order(date: :desc, id: :desc)  }
 end
