7c81e60abaf61ee62f011658bd043d6aeeb77626
Anderson Farias <anderson.faryas@gmail.com>
2016-07-13 22:41:05 -0300

    Fixed email confirmation for new users in production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,6 @@ Rails.application.configure do
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
-    #address: "smtp.gmail.com",
-    #port: 587,
-    #domain: Rails.application.secrets.domain_name,
-    #authentication: "plain",
-    #enable_starttls_auto: true,
-    #user_name: Rails.application.secrets.email_provider_username,
-    #password: Rails.application.secrets.email_provider_password
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
