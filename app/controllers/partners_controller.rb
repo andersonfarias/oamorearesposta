61e5fead4242928eee67e68db01ef7f38a613506
Olivan Aires <olivanaires@gmail.com>
2016-07-27 00:01:14 -0300

    fixed bug when update partner with second contact equals to nil, destroy contact if name is blank

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -48,9 +48,13 @@ class PartnersController < ApplicationController
 
     def update
         authorize @partner
+        update_second_contact_partner(params) if params[:partner][:contact_person_2_attributes][:first_name].blank?
         if @partner.update(partner_params)
             redirect_to @partner, notice: t('controllers.actions.update.success', model: Partner.model_name.human(count: 1))
+        else
+            render 'edit'
         end
+       
     end
 
     def destroy
@@ -69,7 +73,7 @@ class PartnersController < ApplicationController
 
     def partner_params
         params.require(:partner).permit(
-            :id, :fantasy_name, :cnpj, :partner_type, work_areas: [],
+            :id, :fantasy_name, :cnpj, :partner_type, :contact_person_2_id, work_areas: [],
             person_attributes: [
                 :id, :first_name, :email, phones_attributes: [:id, :number],
                                           address_attributes: [:id, :street, :number, :neighborhood, :cep, :city_id, :complement]
@@ -77,6 +81,13 @@ class PartnersController < ApplicationController
             contact_person_1_attributes: [:id, :first_name, :email, phones_attributes: [:id, :number]],
             contact_person_2_attributes: [:id, :first_name, :email, phones_attributes: [:id, :number]]
         )
+    end
+
+    def update_second_contact_partner params
+        partner_id = params[:partner][:contact_person_2_attributes][:id]
+        params[:partner].except!(:contact_person_2_attributes) 
+        params[:partner][:contact_person_2_id] = nil
+        Person.find(partner_id).destroy
     end
 
     def to_autocomplete_items items
