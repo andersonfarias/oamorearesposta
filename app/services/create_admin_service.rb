f2013cac2215cbcd896ae798474c080ea4ead51e
Olivan Aires <olivanaires@gmail.com>
2017-01-24 14:21:59 -0300

    refactoring seed to create user

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,8 +1,11 @@
 class CreateAdminService
 	def call
+		puts 'CREATING ADMIN USER'
+		person = Person.create( first_name: Rails.application.secrets.admin_name, email: Rails.application.secrets.admin_email )
+		partner = Partner.create(person: person, partner_type: :volunteering, work_areas: :education, contact_person_1: person)
 		user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-			puts 'CREATING ADMIN USER'
-			user.person = Person.create( first_name: Rails.application.secrets.admin_name, email: Rails.application.secrets.admin_email )
+			user.person = person
+			user.partner = partner
 			user.password = Rails.application.secrets.admin_password
 			user.password_confirmation = Rails.application.secrets.admin_password
 			if user.confirm! && user.admin!
