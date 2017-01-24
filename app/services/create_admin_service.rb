8b272bdecf9e8ec4686a7545738b0995cffeb429
Olivan Aires <olivanaires@gmail.com>
2017-01-24 14:43:22 -0300

    fix creation of partner

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -2,7 +2,7 @@ class CreateAdminService
 	def call
 		puts 'CREATING ADMIN USER'
 		person = Person.create( first_name: Rails.application.secrets.admin_name, email: Rails.application.secrets.admin_email )
-		partner = Partner.create(person: person, partner_type: :volunteering, work_areas: :education, contact_person_1: person)
+		partner = Partner.create(person: person, partner_type: :volunteering, work_areas: [:education], contact_person_1: person)
 		user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
 			user.person = person
 			user.partner = partner
